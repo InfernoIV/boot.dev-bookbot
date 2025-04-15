c9ce4163cc41b13b2430482c3850e8ace28a5b6b
Joost <github@joostnieuweboer.nl>
2025-04-15 09:46:05 +0000

    Added cli command instead of hardcoded path

--- main.py
+++ main.py
@@ -3,11 +3,22 @@
 
 #imports
 from stats import count_words, count_characters, sort_characters
+import sys
 
 #main function
 def main() :
+    #if not correct arguments
+    if len(sys.argv) != 2:
+        #print help message
+        print("Usage: python3 main.py <path_to_book>")
+        #stop
+        sys.exit(1)
+        #needed?
+        return
+    
+    
     #define the path of the book
-    path_to_file = "books/frankenstein.txt"
+    path_to_file = sys.argv[1] #"books/frankenstein.txt"
     print_report(path_to_file)
 
 

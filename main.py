f7bdd377e46c4b6e52a76f158369ee07de09c0a4
Joost <github@joostnieuweboer.nl>
2025-04-15 08:03:37 +0000

    Added function to get book text

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+#main function
+def main() :
+    #define the path of the book
+    path = "./books/frankenstein.txt"
+    #get the content of the book
+    book_text = get_book_text(path)
+    #print the text to console
+    print(book_text)
+
+#function to get the content of a file
+def get_book_text(path_to_file) :
+    #open the file
+    with open(path_to_file) as f:
+        # f is a file object
+        file_contents = f.read()
+    return file_contents
+
+main()
